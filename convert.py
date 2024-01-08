63670051319d0f3fb48f2a1aa4c10121292ec7ec
uberwow <77true@rambler.ru>
2024-01-08 11:40:17 +0400

    Update convert.py
    Added PDF export

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import json
 import os
 import argparse
 from datetime import datetime
+from markdown_pdf import MarkdownPdf, Section
 
 
 def get_conversation(node_id, mapping, list):
@@ -30,7 +31,7 @@ def main(input_file, output_dir, use_date_folders):
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    with open(input_file) as f:
+    with open(input_file, encoding="utf-8") as f:
         data = json.loads(f.read())
         for item in data:
             title = item["title"].replace("/","_").replace('"','')
@@ -39,7 +40,8 @@ def main(input_file, output_dir, use_date_folders):
             root_node_id = [node_id for node_id, node in item['mapping'].items() if node['parent'] is None][0]
             list = []
             get_conversation(root_node_id, item['mapping'], list)
-
+            date_iso = datetime.fromtimestamp(item["create_time"]).date().isoformat()
+            
             # Handle the date-based folder structure
             date_folder = ''
             if use_date_folders:
@@ -49,14 +51,25 @@ def main(input_file, output_dir, use_date_folders):
                 if not os.path.isdir(date_folder):
                     os.makedirs(date_folder)
 
-            with open(f'{date_folder if use_date_folders else output_dir}/{title}.md', 'w') as outfile:
-                outfile.write('\n'.join(list))
+            # Create a MarkdownPdf object
+            pdf = MarkdownPdf(toc_level=1)
+
+            # Add the conversation data to the PDF
+            pdf.add_section(Section(title, toc=False))
+            pdf.add_section(Section('\n'.join(list)))
+
+            # Set the properties of the PDF document
+            pdf.meta["title"] = title
+
+            # Save the PDF to file
+            pdf.save(f'{date_folder if use_date_folders else output_dir}/{title}_{date_iso}.pdf')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process conversation data.')
     parser.add_argument('input_file', help='JSON file containing conversations')
-    parser.add_argument('output_dir', help='Directory to save output Markdown files')
+    parser.add_argument('output_dir', help='Directory to save output HTML files')
     parser.add_argument('--use-date-folders', action='store_true', help='Store files under date-based folders')
 
     args = parser.parse_args()
     main(args.input_file, args.output_dir, args.use_date_folders)
+    print("All things done!")
